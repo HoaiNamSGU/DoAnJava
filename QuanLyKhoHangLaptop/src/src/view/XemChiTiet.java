e3ce007d886581a1423754c2f7d5858c7099b39a
sÒwÓs <115623421+NhatNam15151515@users.noreply.github.com>
2024-04-09 22:14:54 +0700

    update pannel Cửa hàng

---
+++ QuanLyKhoHangLaptop/src/src/view/XemChiTiet.java
@@ -0,0 +1,228 @@
+package view;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.text.DecimalFormat;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+
+import model.Laptop;
+
+public class XemChiTiet extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel jlabel_malaptop_1;
+	private JLabel jlabel_tenlaptop1;
+	private JLabel jlabel_cpu1;
+	private JLabel jlabel_gpu1;
+	private JLabel jlabel_ram1;
+	private JLabel jlabel_rom1;
+	private JLabel jlabel_HDH1;
+	private JLabel jlabel_manhinh1;
+	private JLabel jlabel_hang1;
+	private JLabel jlabel_namsanxuat1;
+	private JLabel jlabel_soluong1;
+	private JLabel jlabel_gia1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new XemChiTiet();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public XemChiTiet() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 640, 605);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Chi tiết sản phẩm");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel jlabel_chitiet = new JLabel("Chi tiết sản phẩm");
+		jlabel_chitiet.setForeground(new Color(0, 128, 192));
+		jlabel_chitiet.setHorizontalAlignment(SwingConstants.CENTER);
+		jlabel_chitiet.setFont(new Font("Arial", Font.BOLD, 26));
+		jlabel_chitiet.setBounds(151, 10, 330, 50);
+		contentPane.add(jlabel_chitiet);
+		
+		JLabel jlabel_malaptop = new JLabel("Mã laptop :");
+		jlabel_malaptop.setForeground(new Color(0, 128, 192));
+		jlabel_malaptop.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_malaptop.setBounds(26, 78, 96, 30);
+		contentPane.add(jlabel_malaptop);
+		
+		JLabel jlabel_tenlaptop = new JLabel("Ten laptop :");
+		jlabel_tenlaptop.setForeground(new Color(0, 128, 192));
+		jlabel_tenlaptop.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_tenlaptop.setBounds(26, 118, 96, 30);
+		contentPane.add(jlabel_tenlaptop);
+		
+		JLabel jlabel_CPU = new JLabel("CPU :");
+		jlabel_CPU.setForeground(new Color(0, 128, 192));
+		jlabel_CPU.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_CPU.setBounds(26, 158, 96, 30);
+		contentPane.add(jlabel_CPU);
+		
+		JLabel jlabel_GPU = new JLabel("GPU :");
+		jlabel_GPU.setForeground(new Color(0, 128, 192));
+		jlabel_GPU.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_GPU.setBounds(26, 198, 96, 30);
+		contentPane.add(jlabel_GPU);
+		
+		JLabel jlabel_ram = new JLabel("Ram  :");
+		jlabel_ram.setForeground(new Color(0, 128, 192));
+		jlabel_ram.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_ram.setBounds(26, 238, 96, 30);
+		contentPane.add(jlabel_ram);
+		
+		JLabel jlabel_rom = new JLabel("Rom  :");
+		jlabel_rom.setForeground(new Color(0, 128, 192));
+		jlabel_rom.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_rom.setBounds(26, 278, 96, 30);
+		contentPane.add(jlabel_rom);
+		
+		JLabel jlabel_HDH = new JLabel("Hệ điều hành :");
+		jlabel_HDH.setForeground(new Color(0, 128, 192));
+		jlabel_HDH.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_HDH.setBounds(26, 318, 123, 30);
+		contentPane.add(jlabel_HDH);
+		
+		JLabel jlabel_manhinh = new JLabel("Màn hình :");
+		jlabel_manhinh.setForeground(new Color(0, 128, 192));
+		jlabel_manhinh.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_manhinh.setBounds(26, 358, 96, 30);
+		contentPane.add(jlabel_manhinh);
+		
+		JLabel jlabel_hang = new JLabel("Hãng :");
+		jlabel_hang.setForeground(new Color(0, 128, 192));
+		jlabel_hang.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_hang.setBounds(26, 398, 96, 30);
+		contentPane.add(jlabel_hang);
+		
+		JLabel jlabel_namsanxuat = new JLabel("Năm sản xuất :");
+		jlabel_namsanxuat.setForeground(new Color(0, 128, 192));
+		jlabel_namsanxuat.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_namsanxuat.setBounds(26, 438, 123, 30);
+		contentPane.add(jlabel_namsanxuat);
+		
+		JLabel jlabel_soluong = new JLabel("Số lượng :");
+		jlabel_soluong.setForeground(new Color(0, 128, 192));
+		jlabel_soluong.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_soluong.setBounds(26, 478, 96, 30);
+		contentPane.add(jlabel_soluong);
+		
+		JLabel jlabel_gia = new JLabel("Giá :");
+		jlabel_gia.setForeground(new Color(0, 128, 192));
+		jlabel_gia.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_gia.setBounds(26, 518, 96, 30);
+		contentPane.add(jlabel_gia);
+		
+		jlabel_malaptop_1 = new JLabel("");
+		jlabel_malaptop_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_malaptop_1.setBounds(195, 78, 388, 30);
+		contentPane.add(jlabel_malaptop_1);
+		
+		jlabel_tenlaptop1 = new JLabel("");
+		jlabel_tenlaptop1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_tenlaptop1.setBounds(195, 118, 388, 30);
+		contentPane.add(jlabel_tenlaptop1);
+		
+		jlabel_cpu1 = new JLabel("");
+		jlabel_cpu1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_cpu1.setBounds(195, 158, 388, 30);
+		contentPane.add(jlabel_cpu1);
+		
+		jlabel_gpu1 = new JLabel("");
+		jlabel_gpu1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_gpu1.setBounds(195, 198, 388, 30);
+		contentPane.add(jlabel_gpu1);
+		
+		jlabel_ram1 = new JLabel("");
+		jlabel_ram1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_ram1.setBounds(195, 238, 388, 30);
+		contentPane.add(jlabel_ram1);
+		
+		jlabel_rom1 = new JLabel("");
+		jlabel_rom1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_rom1.setBounds(195, 278, 388, 30);
+		contentPane.add(jlabel_rom1);
+		
+		jlabel_HDH1 = new JLabel("");
+		jlabel_HDH1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_HDH1.setBounds(195, 318, 388, 30);
+		contentPane.add(jlabel_HDH1);
+		
+		jlabel_manhinh1 = new JLabel("");
+		jlabel_manhinh1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_manhinh1.setBounds(195, 358, 388, 30);
+		contentPane.add(jlabel_manhinh1);
+		
+		jlabel_hang1 = new JLabel("");
+		jlabel_hang1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_hang1.setBounds(195, 398, 388, 30);
+		contentPane.add(jlabel_hang1);
+		
+		jlabel_namsanxuat1 = new JLabel("");
+		jlabel_namsanxuat1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_namsanxuat1.setBounds(195, 438, 388, 30);
+		contentPane.add(jlabel_namsanxuat1);
+		
+		jlabel_soluong1 = new JLabel("");
+		jlabel_soluong1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_soluong1.setBounds(195, 478, 388, 30);
+		contentPane.add(jlabel_soluong1);
+		
+		jlabel_gia1 = new JLabel("");
+		jlabel_gia1.setFont(new Font("Arial", Font.PLAIN, 18));
+		jlabel_gia1.setBounds(195, 518, 388, 30);
+		contentPane.add(jlabel_gia1);
+		
+		
+		
+		
+
+		this.setVisible(true);
+		this.setResizable(false);
+		
+	}
+	
+	public void setJLabel(Laptop  lt)
+	{
+		jlabel_malaptop_1.setText(lt.getMaLaptop());
+		jlabel_tenlaptop1.setText(lt.getTenLaptop());
+		jlabel_cpu1.setText(lt.getCPU());
+		jlabel_gpu1.setText(lt.getGPU());
+		jlabel_HDH1.setText(lt.getHeDieuHanh());
+		jlabel_manhinh1.setText(lt.getManHinh());
+		jlabel_ram1.setText(lt.getRam());
+		jlabel_rom1.setText(lt.getRom());
+		jlabel_namsanxuat1.setText(lt.getNamSanXuat()+"");
+		jlabel_soluong1.setText(lt.getSoLuong()+"");
+		DecimalFormat df = new DecimalFormat("#");
+	    String gia = df.format(lt.getGia());
+	    jlabel_gia1.setText(gia+" vnd");
+	    jlabel_hang1.setText(lt.getHang());
+	}
+}
---
+++ QuanLyKhoHangLaptop/src/src/view/mainView.java
@@ -0,0 +1,432 @@
+
+package view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+import controller.mainActionlistener;
+import controller.mainMouseListener;
+import dao.CuaHangDAO;
+import dao.LaptopDAO;
+import model.CuaHang;
+import model.Laptop;
+import view.CuaHang_View;
+
+public class mainView extends JFrame {
+
+	public JTextField textField;
+	public JLabel jlabel_sanpham;
+	public JLabel jlabel_nhacungcap;
+	public JLabel jlabel_nhaphang;
+	public JLabel jlabel_phieunhap;
+	public JLabel jlabel_xuathang;
+	public JLabel jlabel_phieuxuat;
+	public JLabel jlabel_cuahang;
+	public JLabel jlabel_taikhoan;
+	public JLabel jlabel_thongke;
+	public JLabel jlabel_doiThongtin;
+	public JLabel jlabel_dangxuat;
+	public JButton jbutton_refesh;
+	public Object data[][];
+	public JTable table;
+	public DefaultTableModel model;
+	public JScrollPane scrollPane;
+	public JComboBox comboBox_search;
+	public JButton jbutton_Sreach;
+	public CuaHang_View CuaHang= new CuaHang_View();
+
+	public mainView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1400, 800);
+		this.setLocationRelativeTo(null);
+		this.setTitle("màn hình chính");
+		getContentPane().setLayout(null);
+
+		JPanel panel_menu = new JPanel();
+		panel_menu.setBounds(0, 0, 197, 763);
+		panel_menu.setBackground(new Color(0, 128, 192));
+		getContentPane().add(panel_menu);
+		panel_menu.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Hi !");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNewLabel.setBounds(63, 45, 36, 24);
+		panel_menu.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Admin");
+		lblNewLabel_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(52, 79, 68, 24);
+		panel_menu.add(lblNewLabel_1);
+
+		jlabel_sanpham = new JLabel("Sản phẩm");
+		jlabel_sanpham.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_sanpham.setForeground(new Color(0, 0, 0));
+		jlabel_sanpham.setBounds(26, 158, 148, 37);
+		ImageIcon icon_sanpham = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\product.png");
+		jlabel_sanpham.setIcon(icon_sanpham);
+		panel_menu.add(jlabel_sanpham);
+
+		jlabel_nhacungcap = new JLabel("Nhà cung cấp");
+		jlabel_nhacungcap.setForeground(new Color(0, 0, 0));
+		jlabel_nhacungcap.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_nhacungcap.setBounds(26, 205, 161, 37);
+		ImageIcon icon_nhaCungCap = new ImageIcon(
+				"D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\supplier.png");
+		jlabel_nhacungcap.setIcon(icon_nhaCungCap);
+		panel_menu.add(jlabel_nhacungcap);
+
+		jlabel_nhaphang = new JLabel("Nhập hàng");
+		jlabel_nhaphang.setForeground(new Color(0, 0, 0));
+		jlabel_nhaphang.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_nhaphang.setBounds(26, 252, 148, 37);
+		ImageIcon icon_nhaphang = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\nhaphang.png");
+		jlabel_nhaphang.setIcon(icon_nhaphang);
+		panel_menu.add(jlabel_nhaphang);
+
+		jlabel_phieunhap = new JLabel("Phiếu nhập");
+		jlabel_phieunhap.setForeground(new Color(0, 0, 0));
+		jlabel_phieunhap.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_phieunhap.setBounds(26, 299, 148, 37);
+		ImageIcon icon_phieunhap = new ImageIcon(
+				"D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\phieunhap.png");
+		jlabel_phieunhap.setIcon(icon_phieunhap);
+		panel_menu.add(jlabel_phieunhap);
+
+		jlabel_xuathang = new JLabel("Xuất hàng");
+		jlabel_xuathang.setForeground(new Color(0, 0, 0));
+		jlabel_xuathang.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_xuathang.setBounds(26, 346, 148, 37);
+		ImageIcon icon_xuathang = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\xuathang.png");
+		jlabel_xuathang.setIcon(icon_xuathang);
+		panel_menu.add(jlabel_xuathang);
+
+		jlabel_phieuxuat = new JLabel("Phiếu xuất");
+		jlabel_phieuxuat.setForeground(new Color(0, 0, 0));
+		jlabel_phieuxuat.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_phieuxuat.setBounds(26, 393, 148, 37);
+		ImageIcon icon_phieuxuat = new ImageIcon(
+				"D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\phieuxuat.png");
+		jlabel_phieuxuat.setIcon(icon_phieuxuat);
+		panel_menu.add(jlabel_phieuxuat);
+
+		jlabel_cuahang = new JLabel("Cửa hàng");
+		jlabel_cuahang.setForeground(new Color(0, 0, 0));
+		jlabel_cuahang.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_cuahang.setBounds(26, 440, 148, 37);
+		ImageIcon icon_cuahang = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\store.png");
+		jlabel_cuahang.setIcon(icon_cuahang);
+		panel_menu.add(jlabel_cuahang);
+
+		jlabel_taikhoan = new JLabel("Tài khoản");
+		jlabel_taikhoan.setForeground(new Color(0, 0, 0));
+		jlabel_taikhoan.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_taikhoan.setBounds(26, 487, 148, 37);
+		ImageIcon icon_taikhoan = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\taikhoan.png");
+		jlabel_taikhoan.setIcon(icon_taikhoan);
+		panel_menu.add(jlabel_taikhoan);
+
+		jlabel_thongke = new JLabel("Thống kê");
+		jlabel_thongke.setForeground(new Color(0, 0, 0));
+		jlabel_thongke.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_thongke.setBounds(26, 534, 148, 37);
+		ImageIcon icon_thongke = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\thongke.png");
+		jlabel_thongke.setIcon(icon_thongke);
+		panel_menu.add(jlabel_thongke);
+
+		jlabel_doiThongtin = new JLabel("Đổi thông tin");
+		jlabel_doiThongtin.setForeground(new Color(0, 0, 0));
+		jlabel_doiThongtin.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_doiThongtin.setBounds(26, 641, 148, 37);
+		ImageIcon icon_doithongtin = new ImageIcon(
+				"D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\thongtin.png");
+		jlabel_doiThongtin.setIcon(icon_doithongtin);
+		panel_menu.add(jlabel_doiThongtin);
+
+		jlabel_dangxuat = new JLabel("Đăng xuất");
+		jlabel_dangxuat.setForeground(new Color(0, 0, 0));
+		jlabel_dangxuat.setFont(new Font("Arial", Font.PLAIN, 20));
+		jlabel_dangxuat.setBounds(26, 688, 148, 37);
+		ImageIcon icon_dangxuat = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\dangxuat.png");
+		jlabel_dangxuat.setIcon(icon_dangxuat);
+		panel_menu.add(jlabel_dangxuat);
+		
+		/*scrollPane = new JScrollPane();
+		scrollPane.setBounds(207, 161, 1189, 602);
+		getContentPane().add(scrollPane);*/
+
+		/*
+		 * Laptop tmp = new Laptop(); tmp.setMaLaptop("MSGB15"); Laptop lt =
+		 * LaptopDAO.getintance().selectById(tmp);
+		 * 
+		 * String column[] = { "Id", "Tên laptop", "Số lượng", "Giá", "CPU", "Ram",
+		 * "Bộ nhớ", "Hệ điều hành" }; Object data[][] = {}; JTable table = new
+		 * JTable(data, column); table.setModel(new DefaultTableModel( new Object[][] {
+		 * {lt.getMaLaptop(),lt.getTenLaptop(),lt.getSoLuong(),lt.getGia(),lt.getCPU(),
+		 * lt.getRam(),lt.getRom(),lt.getHeDieuHanh()} }, new String[] { "Id",
+		 * "Tên laptop", "Số lượng", "Giá", "CPU", "Ram", "Bộ nhớ", "Hệ điều hành" } ));
+		 */
+		/*table=SanPham();
+		
+		JTableHeader header = table.getTableHeader();
+		header.setFont(new Font("Arial", Font.BOLD, 14));
+		table.setFont(new Font("Arial", Font.PLAIN, 14));
+		scrollPane.setViewportView(table);
+
+		JPanel panel_border_chucNang = new JPanel();
+		panel_border_chucNang.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Chức năng",
+				TitledBorder.LEFT, TitledBorder.ABOVE_TOP, new Font("Arial", Font.ITALIC, 14), new Color(0, 0, 0)));
+		panel_border_chucNang.setBounds(207, 10, 421, 81);
+		getContentPane().add(panel_border_chucNang);
+		panel_border_chucNang.setLayout(null);
+
+		ImageIcon icon_add = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\plus.png");
+		JLabel jlabel_them = new JLabel("Thêm", icon_add, JLabel.CENTER);
+		jlabel_them.setFont(new Font("Arial", Font.PLAIN, 12));
+		jlabel_them.setBounds(10, 21, 44, 56);
+		jlabel_them.setVerticalTextPosition(JLabel.BOTTOM);
+		jlabel_them.setHorizontalTextPosition(JLabel.CENTER);
+		panel_border_chucNang.add(jlabel_them);
+
+		ImageIcon icon_pencil = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\pencil.png");
+		JLabel jlabel_sua = new JLabel("Sửa", icon_pencil, JLabel.CENTER);
+		jlabel_sua.setFont(new Font("Arial", Font.PLAIN, 12));
+		jlabel_sua.setBounds(109, 21, 59, 56);
+		jlabel_sua.setVerticalTextPosition(JLabel.BOTTOM);
+		jlabel_sua.setHorizontalTextPosition(JLabel.CENTER);
+		panel_border_chucNang.add(jlabel_sua);
+
+		ImageIcon icon_remove = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\remove.png");
+		JLabel jlabel_xoa = new JLabel("Xóa", icon_remove, JLabel.CENTER);
+		jlabel_xoa.setFont(new Font("Arial", Font.PLAIN, 12));
+		jlabel_xoa.setBounds(67, 21, 45, 50);
+		jlabel_xoa.setVerticalTextPosition(JLabel.BOTTOM);
+		jlabel_xoa.setHorizontalTextPosition(JLabel.CENTER);
+		panel_border_chucNang.add(jlabel_xoa);
+
+		ImageIcon icon_eye = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\eye.png");
+		JLabel jlabel_xemCT = new JLabel("Xem chi tiết", icon_eye, JLabel.CENTER);
+		jlabel_xemCT.setFont(new Font("Arial", Font.PLAIN, 12));
+		jlabel_xemCT.setBounds(165, 21, 68, 50);
+		jlabel_xemCT.setVerticalTextPosition(JLabel.BOTTOM);
+		jlabel_xemCT.setHorizontalTextPosition(JLabel.CENTER);
+		panel_border_chucNang.add(jlabel_xemCT);
+
+		ImageIcon icon_nhapExcel = new ImageIcon(
+				"D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\nhapExcel.png");
+		JLabel jlabel_nhapExcel = new JLabel("Nhập Excel", icon_nhapExcel, JLabel.CENTER);
+		jlabel_nhapExcel.setFont(new Font("Arial", Font.PLAIN, 12));
+		jlabel_nhapExcel.setBounds(265, 21, 68, 50);
+		jlabel_nhapExcel.setVerticalTextPosition(JLabel.BOTTOM);
+		jlabel_nhapExcel.setHorizontalTextPosition(JLabel.CENTER);
+		panel_border_chucNang.add(jlabel_nhapExcel);
+
+		ImageIcon icon_Excel = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\xuatExcel.png");
+		JLabel jlabel_Excel = new JLabel("Xuất Excel", icon_Excel, JLabel.CENTER);
+		jlabel_Excel.setFont(new Font("Arial", Font.PLAIN, 12));
+		jlabel_Excel.setBounds(343, 21, 68, 50);
+		jlabel_Excel.setVerticalTextPosition(JLabel.BOTTOM);
+		jlabel_Excel.setHorizontalTextPosition(JLabel.CENTER);
+		panel_border_chucNang.add(jlabel_Excel);
+
+		JPanel panel_border_timKiem = new JPanel();
+		panel_border_timKiem.setLayout(null);
+		panel_border_timKiem.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Tìm kiếm",
+				TitledBorder.LEFT, TitledBorder.ABOVE_TOP, new Font("Arial", Font.ITALIC, 14), new Color(0, 0, 0)));
+		panel_border_timKiem.setBounds(766, 10, 620, 81);
+		getContentPane().add(panel_border_timKiem);
+		
+		mainActionlistener ac = new mainActionlistener(this);
+		String[] combo = { "Tất cả", "Windows", "MacOS" };
+		comboBox_search = new JComboBox(combo);
+		comboBox_search.addActionListener(ac);
+		comboBox_search.setBackground(new Color(255, 255, 255));
+		comboBox_search.setFont(new Font("Arial", Font.PLAIN, 14));
+		comboBox_search.setBounds(10, 36, 95, 21);
+		panel_border_timKiem.add(comboBox_search);
+		
+		textField = new JTextField();
+		textField.setBounds(131, 31, 286, 33);
+		panel_border_timKiem.add(textField);
+		textField.setColumns(10);
+
+		jbutton_Sreach = new JButton("Tìm kiếm");
+		jbutton_Sreach.addActionListener(ac);
+		jbutton_Sreach.setBackground(new Color(255, 255, 255));
+		jbutton_Sreach.setBounds(502, 31, 95, 33);
+		panel_border_timKiem.add(jbutton_Sreach);
+
+		ImageIcon icon_refresh = new ImageIcon("D:\\java_setup\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\arrow.png");
+		jbutton_refesh = new JButton(icon_refresh);
+		jbutton_refesh.setBackground(new Color(255, 255, 255));
+		jbutton_refesh.setBounds(427, 31, 42, 33);
+		panel_border_timKiem.add(jbutton_refesh);
+		
+		JPanel panel_center = new JPanel();
+		panel_center.setBounds(197, 0, 1189, 763);
+		getContentPane().add(panel_center);
+
+		
+		jbutton_refesh.addActionListener(ac);
+
+		mainMouseListener mouse = new mainMouseListener(this);
+		jlabel_sanpham.addMouseListener(mouse);
+		jlabel_nhacungcap.addMouseListener(mouse);
+		jlabel_nhaphang.addMouseListener(mouse);
+		jlabel_phieunhap.addMouseListener(mouse);
+		jlabel_xuathang.addMouseListener(mouse);
+		jlabel_phieuxuat.addMouseListener(mouse);
+		jlabel_cuahang.addMouseListener(mouse);
+		jlabel_taikhoan.addMouseListener(mouse);
+		jlabel_thongke.addMouseListener(mouse);
+		jlabel_doiThongtin.addMouseListener(mouse);
+		jlabel_dangxuat.addMouseListener(mouse);
+		jlabel_them.addMouseListener(mouse);
+		jlabel_Excel.addMouseListener(mouse);
+		jlabel_sua.addMouseListener(mouse);
+		jlabel_nhapExcel.addMouseListener(mouse);
+		jlabel_xoa.addMouseListener(mouse);
+		jlabel_xemCT.addMouseListener(mouse);*/
+		this.add(CuaHang);
+		this.setVisible(true);
+		this.setResizable(false);
+		
+	}
+
+	public JTable SanPham() {
+		ArrayList<Laptop> listLaptop = LaptopDAO.getintance().selectAll();
+
+		data = new Object[listLaptop.size()][8];
+		for (int i = 0; i < listLaptop.size(); i++) {
+			Laptop t = listLaptop.get(i);
+			data[i][0] = t.getMaLaptop();
+			data[i][1] = t.getTenLaptop();
+			data[i][2] = t.getSoLuong();
+			DecimalFormat df = new DecimalFormat("#");
+		    String formattedNumber = df.format(t.getGia());
+			data[i][3] = formattedNumber;
+			data[i][4] = t.getCPU();
+			data[i][5] = t.getRam();
+			data[i][6] = t.getRom();
+			data[i][7] = t.getHeDieuHanh();
+		}
+		String column[] = { "Id", "Tên laptop", "Số lượng", "Giá", "CPU", "Ram", "Bộ nhớ", "Hệ điều hành" };
+		model = new DefaultTableModel(data, column);
+		table = new JTable(model);
+
+		setKhoangCachTable();
+		return table;
+	}
+
+	public JTable CuaHang() {
+		ArrayList<CuaHang> ListCuaHang=CuaHangDAO.getintance().selectAll();
+		data=new Object[ListCuaHang.size()][4];
+		for (int i=0;i<ListCuaHang.size();i++) {
+			CuaHang CH=ListCuaHang.get(i);
+			data[i][0]=CH.getMaCH();
+			data[i][1]=CH.getTenCH();
+			data[i][2]=CH.getDiaChi();
+			data[i][3]=CH.getSDT();
+			
+		}
+		String column[] = { "Mã CH", "Tên Cửa Hàng", "Địa Chỉ", "Điện Thoại"};
+		model = new DefaultTableModel(data, column);
+		table = new JTable(model);
+		table.getColumnModel().getColumn(0).setPreferredWidth(10);
+		table.getColumnModel().getColumn(1).setPreferredWidth(400);
+		table.getColumnModel().getColumn(1).setMinWidth(20);
+		table.getColumnModel().getColumn(2).setPreferredWidth(400);
+		table.getColumnModel().getColumn(3).setPreferredWidth(15);
+		return table;
+	}
+
+	public void updateTable(JTable newTable) {
+		getContentPane().remove(scrollPane);
+		scrollPane.setBounds(207, 161, 1189, 602);
+		getContentPane().add(scrollPane);
+		JTableHeader header = newTable.getTableHeader();
+		header.setFont(new Font("Arial", Font.BOLD, 14));
+		newTable.setFont(new Font("Arial", Font.PLAIN, 14));
+		scrollPane.setViewportView(newTable);
+		// Cập nhật lại giao diện
+		this.revalidate();
+		this.repaint();
+	}
+	
+	public void updateTableData( ArrayList<Laptop> listLaptop) {
+	  
+	    // Tạo lại mảng dữ liệu và cập nhật bảng
+	    Object newData[][] = new Object[listLaptop.size()][8];
+	    for (int i = 0; i < listLaptop.size(); i++) {
+	        Laptop t = listLaptop.get(i);
+	        newData[i][0] = t.getMaLaptop();
+	        newData[i][1] = t.getTenLaptop();
+	        newData[i][2] = t.getSoLuong();
+	        DecimalFormat df = new DecimalFormat("#");
+	        String formattedNumber = df.format(t.getGia());
+	        newData[i][3] = formattedNumber;
+	        newData[i][4] = t.getCPU();
+	        newData[i][5] = t.getRam();
+	        newData[i][6] = t.getRom();
+	        newData[i][7] = t.getHeDieuHanh();
+	    }
+	    String column[] = { "Id", "Tên laptop", "Số lượng", "Giá", "CPU", "Ram", "Bộ nhớ", "Hệ điều hành" };
+	    // Cập nhật mô hình của bảng
+	    model.setDataVector(newData, column);
+	    setKhoangCachTable();
+	    // Cập nhật lại giao diện
+	    this.revalidate();
+	    this.repaint();
+	}
+	
+	public void setKhoangCachTable()
+	{
+		table.getColumnModel().getColumn(0).setPreferredWidth(52);
+		table.getColumnModel().getColumn(1).setPreferredWidth(200);
+		table.getColumnModel().getColumn(1).setMinWidth(15);
+		table.getColumnModel().getColumn(2).setPreferredWidth(30);
+		table.getColumnModel().getColumn(3).setPreferredWidth(40);
+		table.getColumnModel().getColumn(4).setPreferredWidth(93);
+		table.getColumnModel().getColumn(5).setPreferredWidth(35);
+		table.getColumnModel().getColumn(6).setPreferredWidth(50);
+		table.getColumnModel().getColumn(7).setPreferredWidth(100);
+	}
+	
+	public ArrayList<Laptop> select_search_laptop(String s)
+	{
+		s=s.toUpperCase();
+		ArrayList<Laptop> list = LaptopDAO.getintance().selectAll();
+		ArrayList<Laptop> arr = new ArrayList<Laptop>();
+		for (Laptop laptop : list) {
+			String tmp = laptop.getMaLaptop()+" "+laptop.getTenLaptop()+" "+laptop.getCPU()+" "+laptop.getGPU()+" "+laptop.getRam()+" "
+			    +laptop.getRom()+" "+laptop.getManHinh()+" "+laptop.getHeDieuHanh()+" "+laptop.getHang()+" "+laptop.getNamSanXuat()
+			    +" "+laptop.getSoLuong()+" "+laptop.getGia();
+			tmp=tmp.toUpperCase();
+			if(tmp.indexOf(s)!=-1)
+			{
+				arr.add(laptop);
+			}
+		}
+		return arr;
+	}
+}
+
