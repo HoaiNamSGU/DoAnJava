c9a09deac8f3259dc14c89f052e8398904ada6b5
DuongTrong35 <109820331+DuongTrong35@users.noreply.github.com>
2024-05-04 00:18:43 +0700

    Add files via upload

--- QuanLyKhoHangLaptop/src/controller/Controllernhaphang.java
+++ QuanLyKhoHangLaptop/src/controller/Controllernhaphang.java
@@ -1,14 +1,19 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
+import view.Phieunhap;
 import view.NhapHang;
 
 public class Controllernhaphang {
@@ -23,37 +28,67 @@ public class Controllernhaphang {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				JLabel check = (JLabel) e.getSource();
-
+				
 			}
 		});
 	}
 
+	public static void addMouseListener(JTable label, NhapHang nh) {
+		label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JTable check = (JTable) e.getSource();
+				if(check==nh.table) {
+					nh.themsanpham();
+				}else if(check==nh.table1) {
+					nh.clickvaotable1();
+				}
+			}
+		});
+	}
+	
+	public static void addActionListener(JComboBox label, NhapHang nh) {
+		label.addActionListener(new ActionListener() {
+         	public void actionPerformed(ActionEvent e) {
+				JComboBox check = (JComboBox) e.getSource();
+         		
+         		}
+         });
+	}
+	
 	public static void addKeyListener(JTextField jt, NhapHang nh) {
 		jt.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
-				if (e.getSource() == nh.jtext_tim) {
+				JTextField check=(JTextField)e.getSource();
+				if(check==nh.jt_soluong) {
+					nh.hiengiakhithem();
+				}else if (check==nh.jt_otimkiem){
 					nh.thanhtimkiem();
 				}
 			}
 		});
 	}
 
+	
+	
 	public static void ActionListener(JButton jt, NhapHang nh) {
 		jt.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (e.getSource() == nh.btn_them) {
-					nh.themhang();
-				} else if (e.getSource() == nh.btn_xoasp) {
-					nh.xoasanpham();
-				} else if (e.getSource() == nh.btn_lammoi) {
+				JButton check=(JButton)e.getSource();
+				if(check==nh.btn_themsp) {
+					nh.themvaobang();
+				}else if(check==nh.btn_nhaphang) {
+					nh.nhaphang();
+				}else if(check ==nh.btn_quaylai) {
+					nh.nutquaylai();
+				}else if(check==nh.btn_timkiem) {
 					nh.lammoithanh();
-
-				} else if (e.getSource() == nh.btn_nhaphang) {
-					nh.NhapHang();
-				} else if (e.getSource() == nh.btn_suasoluong) {
-					nh.suasl();
+				}else if(check==nh.btn_xoasp) {
+					nh.xoasanphamthem();
+				}else if(check==nh.btn_suasp) {
+					nh.suasanphamthem();
 				}
 			}
 		});
