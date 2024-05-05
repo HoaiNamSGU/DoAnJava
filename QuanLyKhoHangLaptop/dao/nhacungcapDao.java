7a741497867f984910e051ca96ddf0c484c616a0
sÒwÓs <115623421+NhatNam15151515@users.noreply.github.com>
2024-05-05 22:24:37 +0700

    done

---
+++ QuanLyKhoHangLaptop/dao/nhacungcapDao.java
@@ -0,0 +1,258 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import database.JDBCUtil;
+import model.NhaCungCap;
+
+public class nhacungcapDao implements DAOInterface<NhaCungCap> {
+	public static nhacungcapDao getInstance() {
+		return new nhacungcapDao();
+	}
+
+	public NhaCungCap chon1nhacc(String tennc) {
+		NhaCungCap ketqua = null;
+		try {
+//		BƯỚC 1: TẠO KẾT NỐI ĐẾN CSDL
+			Connection con = JDBCUtil.getConnection();
+//		BƯỚC 2: TẠO RA ĐỐI TƯỢNG STATEMENT
+			String sql = "SELECT * FROM nhacungcap WHERE TenNhaCungCap='" + tennc + "'";
+			PreparedStatement pst = con.prepareStatement(sql);
+//		BƯỚC 3: THỰC THI CÂU LỆNH SQL
+//		System.out.println(sql);
+			ResultSet rs = pst.executeQuery(sql);
+			// BƯỚC 4 XỬ LÝ KẾT QUẢ
+//		String manhacungcap, String tennhacungcap, String diachi, String sdt, String email
+			while (rs.next()) {
+				String mncc = rs.getString("MaNhaCungCap");
+				String tm = rs.getString("TenNhaCungCap");
+				String dc = rs.getString("DiaChi");
+				String sdt = rs.getString("SDT");
+				String em = rs.getString("Email");
+				int isdelete=rs.getInt("isDelete");
+				ketqua = new NhaCungCap(mncc, tm, dc, sdt, em,isdelete);
+			}
+//		BƯỚC 5: NGẮT KẾT NỐI
+			JDBCUtil.closeConnection(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ketqua;
+	}
+
+	@Override
+	public int insert(NhaCungCap t) {
+		int ketqua = 0;
+		try {
+			// TODO Auto-generated method stub
+//		BƯỚC 1: TẠO KẾT NỐI ĐẾN CSDL
+			Connection con = JDBCUtil.getConnection();
+
+//		BƯỚC 2: TẠO RA ĐỐI TƯỢNG STATEMENT
+			String sql = "INSERT INTO nhacungcap(MaNhaCungCap,TenNhaCungCap,DiaChi,SDT,Email,isDelete)\r\n"
+					+ "VALUES (?,?,?,?,?,?)";
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setString(1, t.getMaNhaCungCap());
+			pst.setString(2, t.getTenNhaCungCap());
+			pst.setString(3, t.getDiaChi());
+			pst.setString(4, t.getSDT());
+			pst.setString(5, t.getEmail());
+			pst.setInt(6, t.getIsDelete());
+//		BƯỚC 3: THỰC THI CÂU LỆNH SQL
+			ketqua = pst.executeUpdate();
+			// BƯỚC 4 XỬ LÝ KẾT QUẢ
+			if (ketqua > 0)
+				System.out.println("Thêm dữ liệu thành công");
+			else
+				System.out.println("Thêm dữ liệu thất bại");
+
+//		BƯỚC 5: NGẮT KẾT NỐI
+			JDBCUtil.closeConnection(con);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ketqua;
+	}
+	public Boolean restoreNCC(String MaNCC) {
+		try {
+
+			String sql = "UPDATE nhacungcap SET isDelete = 0 WHERE MaNhaCungCap = ?";
+			Connection con = JDBCUtil.getConnection();
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setString(1, MaNCC);
+			pst.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	@Override
+	public int update(NhaCungCap t) {
+		// TODO Auto-generated method stub
+		int ketqua = 0;
+		try {
+			// TODO Auto-generated method stub
+//		BƯỚC 1: TẠO KẾT NỐI ĐẾN CSDL
+			Connection con = JDBCUtil.getConnection();
+
+//		BƯỚC 2: TẠO RA ĐỐI TƯỢNG STATEMENT
+			Statement st = con.createStatement();
+			String sql = "UPDATE nhacungcap" + " SET " + " MaNhaCungCap='" + t.getMaNhaCungCap() + "'"
+					+ ", TenNhaCungCap='" + t.getTenNhaCungCap() + "'" + ", DiaChi='" + t.getDiaChi() + "'" + ", SDT='"
+					+ t.getSDT() + "'" + ", Email='" + t.getEmail() + "'"+", isDelete ='"+t.getIsDelete()+"'" + " WHERE MaNhaCungCap='"
+					+ t.getMaNhaCungCap() + "'";
+
+//		BƯỚC 3: THỰC THI CÂU LỆNH SQL
+			ketqua = st.executeUpdate(sql);
+			// BƯỚC 4 XỬ LÝ KẾT QUẢ
+			if (ketqua > 0)
+				System.out.println("Thêm dữ liệu thành công");
+			else
+				System.out.println("Thêm dữ liệu thất bại");
+
+//		BƯỚC 5: NGẮT KẾT NỐI
+			JDBCUtil.closeConnection(con);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ketqua;
+	}
+
+	public int delete1(NhaCungCap t) {
+		// TODO Auto-generated method stub
+		int ketqua = 0;
+		try {
+			// TODO Auto-generated method stub
+//				BƯỚC 1: TẠO KẾT NỐI ĐẾN CSDL
+			Connection con = JDBCUtil.getConnection();
+
+//				BƯỚC 2: TẠO RA ĐỐI TƯỢNG STATEMENT
+			Statement st = con.createStatement();
+			String sql = "DELETE from nhacungcap" + " WHERE MaNhaCungCap='" + t.getMaNhaCungCap() + "'";
+
+//				BƯỚC 3: THỰC THI CÂU LỆNH SQL
+			ketqua = st.executeUpdate(sql);
+			// BƯỚC 4 XỬ LÝ KẾT QUẢ
+			if (ketqua > 0)
+				System.out.println("Thêm dữ liệu thành công");
+			else
+				System.out.println("Thêm dữ liệu thất bại");
+
+//				BƯỚC 5: NGẮT KẾT NỐI
+			JDBCUtil.closeConnection(con);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ketqua;
+	}
+
+	@Override
+	public ArrayList<NhaCungCap> selectAll() {
+		ArrayList<NhaCungCap> ketqua = new ArrayList<NhaCungCap>();
+		try {
+//		BƯỚC 1: TẠO KẾT NỐI ĐẾN CSDL
+			Connection con = JDBCUtil.getConnection();
+//		BƯỚC 2: TẠO RA ĐỐI TƯỢNG STATEMENT
+			String sql = "SELECT * FROM nhacungcap";
+			PreparedStatement pst = con.prepareStatement(sql);
+//		BƯỚC 3: THỰC THI CÂU LỆNH SQL
+//		System.out.println(sql);
+			ResultSet rs = pst.executeQuery(sql);
+			// BƯỚC 4 XỬ LÝ KẾT QUẢ
+//		String manhacungcap, String tennhacungcap, String diachi, String sdt, String email
+			while (rs.next()) {
+				String mncc = rs.getString("MaNhaCungCap");
+				String tm = rs.getString("TenNhaCungCap");
+				String dc = rs.getString("DiaChi");
+				String sdt = rs.getString("SDT");
+				String em = rs.getString("Email");
+				int isDelete = rs.getInt("isDelete");
+				NhaCungCap sp = new NhaCungCap(mncc, tm, dc, sdt, em,isDelete);
+				ketqua.add(sp);
+			}
+//		BƯỚC 5: NGẮT KẾT NỐI
+			JDBCUtil.closeConnection(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ketqua;
+	}
+
+	public NhaCungCap selectById1(NhaCungCap t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<NhaCungCap> selectByCondition(String condition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(String t) {
+		// TODO Auto-generated method stub
+		
+		return 0;
+	}
+	public Boolean isMaNCCExit(String maNCC) {
+		return selectById(maNCC)!=null;
+	}
+	@Override
+	public NhaCungCap selectById(String maNCC) {
+
+		try {
+			//		BƯỚC 1: TẠO KẾT NỐI ĐẾN CSDL
+			Connection con = JDBCUtil.getConnection();
+			//		BƯỚC 2: TẠO RA ĐỐI TƯỢNG STATEMENT
+			String sql = "SELECT * FROM nhacungcap WHERE MaNhaCungCap = ?";
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setString(1, maNCC);
+			//		BƯỚC 3: THỰC THI CÂU LỆNH SQL
+			//		System.out.println(sql);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				String mncc = rs.getString("MaNhaCungCap");
+				String tm = rs.getString("TenNhaCungCap");
+				String dc = rs.getString("DiaChi");
+				String sdt = rs.getString("SDT");
+				String em = rs.getString("Email");
+				int isDelete = rs.getInt("isDelete");
+				return new NhaCungCap(mncc, tm, dc, sdt, em,isDelete);
+			}
+			//		BƯỚC 5: NGẮT KẾT NỐI
+			JDBCUtil.closeConnection(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public int delete(NhaCungCap t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public NhaCungCap selectById(NhaCungCap t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
